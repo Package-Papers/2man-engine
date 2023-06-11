a56c39d94e9e5b4fad3eb41280d48cde0f5b75f4
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-06-11 20:33:35 +0700

    clang-tidy/clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,36 @@
+#include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
-#include <SFML/Graphics.hpp>
 
-#include <imgui-SFML.h>
-#include <imgui.h>
+// #include <imgui-SFML.h>
+// #include <imgui.h>
 #include <iostream>
 
-int main() {
+int main()
+{
     sf::RenderWindow window(sf::VideoMode({1280, 720}), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
-    bool imgui_success = ImGui::SFML::Init(window);
+    // bool imgui_success = ImGui::SFML::Init(window);
 
-    if (!imgui_success) std::cerr << "Failed to initialize imgui.";
-
-    sf::Clock deltaClock;
+    // if (!imgui_success) std::cerr << "Failed to initialize imgui.";
+    sf::Clock delta_clock;
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(event);
-
+            // ImGui::SFML::ProcessEvent(event);
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
+        // ImGui::SFML::Update(window, deltaClock.restart());
 
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        ImGui::ShowDemoWindow();
-
+        // ImGui::ShowDemoWindow();
         window.clear();
-        ImGui::SFML::Render(window);
+
+        // ImGui::SFML::Render(window);
         window.display();
     }
-
-    ImGui::SFML::Shutdown();
-
+    // ImGui::SFML::Shutdown();
     return 0;
 }
