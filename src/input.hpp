c465322e06aad40cc9f7e974a3a9c0a86d97da69
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-07-03 01:30:26 +0700

    feat(input): added input base

---
+++ src/input.hpp
@@ -0,0 +1,34 @@
+
+#pragma once
+#ifndef TME_INPUT
+#define TME_INPUT
+
+#include "SFML/Window/Event.hpp"
+
+////////////////////////////////////
+// Note(Appy): Interface for input devices.
+
+#ifndef __cpp_explicit_this_parameter // Handle traditional syntax.
+
+template <class Derived>
+class Input
+{
+  void handle_event(const sf::Event& event) noexcept
+  {
+    (static_cast<Derived*>(this))->impl(event);
+  }
+};
+
+#else // C++23 syntax
+
+class Input
+{
+  void handle_event(this auto&& self) noexcept
+  {
+    self.impl();
+  }
+};
+
+#endif
+
+#endif /* TME_INPUT */
