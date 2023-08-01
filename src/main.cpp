7d36b4ab91446464093e068f89a61eba10bf94a7
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-08-01 14:28:39 +0700

    feat: doctest integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 #include "common.hpp"
 #include "engine.hpp"
 
+#ifdef NDEBUG
+    #define DOCTEST_CONFIG_DISABLE
+#endif
+
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest/doctest.h>
+
 /**
  * Settings can be defined in 'settings.json'.
  */
 
 #include "settings.hpp"
 
-int main()
+void doctest_run(int argc, char** argv)
 {
+    doctest::Context ctx;
+    ctx.setOption("abort-after", 1);
+    ctx.applyCommandLine(argc, argv);
+    ctx.setOption("no-breaks", true);
+    int res = ctx.run();
+
+    if (ctx.shouldExit())
+    {
+        exit(res);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    // Try running doctest.
+    doctest_run(argc, argv);
+
     const tme::WindowContext default_ctx = {
         TME_WINDOW_WIDTH,
         TME_WINDOW_HEIGHT,
