b8851cefdf13459cf3f0f18bf7ac1918beffda70
Nam Bouchara <93874437+namearth5005@users.noreply.github.com>
2023-09-01 00:50:02 +1000

    feat - Made changes in state_stack.hpp

--- src/state_stack.hpp
+++ src/state_stack.hpp
@@ -16,8 +16,6 @@
 
 #include "settings.hpp"
 
-class StateStack;
-
 namespace states
 {
 enum ID
@@ -29,6 +27,8 @@ enum ID
 };
 }
 
+class StateStack;
+
 class State
 {
   public:
@@ -38,30 +38,43 @@ class State
 
     struct Context
     {
-        Context(sf::RenderWindow& window, TextureHolder& textures, FontHolder& fonts);
-
+        Context()
+        {
+        }
         sf::RenderWindow* window;
         TextureHolder*    textures;
         FontHolder*       fonts;
     };
 
   public:
-    State(StateStack& stack, Context context);
-    virtual ~State();
+    State(StateStack& stack, Context context)
+        : m_state_stack(&stack)
+        , m_context(context)
+    {
+    }
+    virtual ~State()
+    {
+    }
     virtual void draw()                               = 0;
     virtual bool update(sf::Time dt)                  = 0;
     virtual bool handle_event(const sf::Event& event) = 0;
 
-  protected:
+  public:
     void request_stack_push(states::ID stateID);
     void request_stack_pop();
     void request_state_clear();
 
     Context get_context() const;
+
+  private:
+    StateStack* m_state_stack;
+    Context     m_context;
 };
 
 class StateStack : private tme::NonCopyable
 {
+    friend State;
+
   public:
     enum Action
     {
@@ -71,13 +84,40 @@ class StateStack : private tme::NonCopyable
     };
     // template <typename T>
     // void registerState(states::ID stateID);
+    // StateStack(const State::Context& ctx)
+    // : m_context{ctx}
+    // {
+
+    // }
 
     void update(sf::Time dt);
     void draw();
-    void push_state(states::ID stateID);
-    void pop_state();
-    void clear_states();
-    bool is_empty() const;
+    void push_state(State::Ptr state);
+    void pop_state()
+    {
+        m_stack.pop_back();
+    }
+    void clear_states()
+    {
+        m_stack.clear();
+    }
+    bool is_empty() const
+    {
+        return (m_stack.size() == 0);
+    }
+
+    std::size_t size() noexcept
+    {
+        return m_stack.size();
+    }
+    std::size_t num_registered_states() noexcept
+    {
+        return m_factories.size();
+    }
+    std::size_t num_pending_states() noexcept
+    {
+        return m_pending_list.size();
+    }
 
   private:
     struct PendingChange
@@ -85,32 +125,31 @@ class StateStack : private tme::NonCopyable
         Action     action;
         states::ID stateID;
     };
-    std::vector<State::Ptr>    mStack;
-    std::vector<PendingChange> mPendingList;
-    State::Context             mContext;
-
+    std::vector<State::Ptr>    m_stack;
+    std::vector<PendingChange> m_pending_list;
+    State::Context             m_context;
     // Given that we do not want to create all state objects from the beginning,
     // we can instead use factory functions that createa  new state on-demand.
-    std::map<states::ID, std::function<State::Ptr()>> mFactories;
+    std::map<states::ID, std::function<State::Ptr()>> m_factories;
 
+  public:
     // Fully written functions below.
-
     template <typename T>
     void register_state(states::ID stateID)
     {
-        mFactories[stateID] = [this]() { return State::Ptr(new T(*this, mContext)); };
+        m_factories[stateID] = [this]() { return State::Ptr(new T(*this, m_context)); };
     }
 
     State::Ptr create_state(states::ID stateID)
     {
-        auto found = mFactories.find(stateID);
-        assert(found != mFactories.end());
+        auto found = m_factories.find(stateID);
+        assert(found != m_factories.end());
 
         return found->second();
     }
     void handle_event(const sf::Event& event)
     {
-        for (auto itr = mStack.rbegin(); itr != mStack.rend(); ++itr)
+        for (auto itr = m_stack.rbegin(); itr != m_stack.rend(); ++itr)
         {
             if (!(*itr)->handle_event(event))
                 return;
@@ -119,67 +158,86 @@ class StateStack : private tme::NonCopyable
     }
     void apply_pending_changes()
     {
-        for (const PendingChange& change : mPendingList)
+        for (const PendingChange& change : m_pending_list)
         {
             switch (change.action)
             {
                 case Push:
-                    mStack.push_back(create_state(change.stateID));
+                    m_stack.push_back(create_state(change.stateID));
                     break;
                 case Pop:
-                    mStack.pop_back();
+                    m_stack.pop_back();
                     break;
                 case Clear:
-                    mStack.clear();
+                    m_stack.clear();
                     break;
             }
         }
-        mPendingList.clear();
+        m_pending_list.clear();
     }
 };
 
-TEST_CASE("Statestack Initialisation")
+void State::request_stack_push(states::ID stateID)
 {
-    const tme::WindowContext default_ctx = {
-        TME_WINDOW_WIDTH,
-        TME_WINDOW_HEIGHT,
-        TME_WINDOW_TITLE,
-        TME_WINDOW_FRAMES_LIM,
-        int_to_bool(TME_VERTICAL_SYNC),
-        int_to_bool(TME_WINDOW_FULLSCREEN),
+    m_state_stack->m_pending_list.push_back({StateStack::Action::Push, stateID});
+}
+void StateStack::push_state(State::Ptr state)
+{
+    m_stack.push_back(std::move(state));
+}
+
+TEST_CASE("Test")
+{
+    auto stack = StateStack();
+    class MenuState : public State
+    {
+      public:
+        virtual void draw()
+        {
+        }
+        virtual bool update(sf::Time dt)
+        {
+            return true;
+        }
+        virtual bool handle_event(const sf::Event& event)
+        {
+            return true;
+        }
+        MenuState(StateStack& stack, Context context)
+            : State(stack, context)
+        {
+        }
     };
+    stack.register_state<MenuState>(states::MainMenu);
+    SUBCASE("check if the stack is empty after initialising")
+    {
+        CHECK(stack.size() == 0);
+        CHECK(stack.is_empty());
+    }
+    SUBCASE("registering a State to a mFactories")
+    {
 
-    auto engine = tme::Engine(default_ctx);
-    CHECK(true);
+        CHECK(stack.num_registered_states() != 0);
+        stack.register_state<MenuState>(states::MainMenu);
+        CHECK(stack.num_registered_states() == 1);
+    }
+    SUBCASE("Testing for createState function")
+    {
+        auto new_state = stack.create_state(states::MainMenu);
+        auto tmp       = dynamic_cast<MenuState*>(new_state.get());
+        CHECK(tmp != nullptr);
+    }
+    SUBCASE("Testing for apply_pending_changes")
+    {
+        auto new_state = stack.create_state(states::MainMenu);
+        new_state->request_stack_push(states::MainMenu);
+        CHECK(stack.num_pending_states() == 1);
+        stack.apply_pending_changes();
+        CHECK(stack.size() == 1);
+    }
+    SUBCASE("Testing for handle_events")
+    {
+    }
 }
-
-TEST_CASE("Stack Initialisation")
-{
-}
-
-// In process.
-
-// class Application
-// {
-//     public:
-//         void Application::registerStates()
-//         {
-//             mStateStack.registerState<TitleState>(States::TitleScreen);
-//             mStateStack.registerState<MenuState>(States::MainMenu);
-//             mStateStack.registerState<GameState>(States::Game);
-//             mStateStack.registerState<PauseState>(States::Pause);
-//         }
-//         void Application::processInput()
-//         {
-//             while (mWindow.pollEvent(event)) {
-//                 mStateStack.handleEvent(event);
-//             }
-//         }
-//         void Application::update(sf::Time dt)
-//        {
-//            mStateStack.update(dt);
-//        }
-
-// }
 
 #endif /* TME_STATE_STACK */
